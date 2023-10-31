cd9b2915db5ce8d941a662de18d4fafb8f8f83c9
Maria Belen De Franchi <belendefranchi@gmail.com>
2023-10-31 11:13:00 -0300

    defino lógica para inserción de filas en dbFechas

--- index.php
+++ index.php
@@ -16,7 +16,8 @@
     set_time_limit(300);
     ini_set('precision', 15);
 
-//##//
+
+		/* ############################### OBTENER EL ULTIMO ARCHIVO MODIFICADO ############################### */
 		$path= "./basesOrigen";
 		$dir=opendir($path);
 
@@ -27,109 +28,135 @@
 				$files[$file] = filemtime($path."/".$file);
 			}
 		}
-		//Ordena desde el más reciente.
-		arsort ($files);
-		print_r($files);
 
-		$i=0;
-		foreach ($files as $file => $timestamp) {
-			if ( $i < 1 ) {
+		if (!empty($files)) {
+			//Ordena desde el más reciente.
+			arsort ($files);
 
-				preg_match('/(\d+)/', $file, $number);
-				$fileNumber = $number[0];
 
-				echo "<p>" . date("d-m-y H:i:s", $timestamp) . "</p>";
-				echo "<p>$timestamp</p>";
-				echo "<a href=\"$path/$file\">$path/$file</a>";
-				echo "<p>file: $file</p>";
-				echo "<p>fileNumber: $fileNumber</p>";
-				echo "<p>$path/$file</p>";
+			$i=0;
+			foreach ($files as $file => $timestamp) {
+				if ( $i < 1 ) {
+
+					preg_match('/(\d+)/', $file, $number);
+					$fileNumberOrigen = $number[0];
+
+					echo "RUTA: <a href=\"$path/$file\">$path/$file</a>";
+					echo "<p>file: $file</p>";
+					echo "<p>fileNumber: $fileNumberOrigen</p>";
+
+					$dbOrigen = new SQLite3($path . "/" . $file);
+
+					/* ########################## OBTENER EL ULTIMO ARCHIVO INSERTADO EN DBFECHAS ######################### */
+
+					$dbFechas = new SQLite3('basesDestino/dbFechas.db');
+
+					// Consulta para verificar la existencia de la tabla 'data'
+					$tableExistsQuery = "SELECT name FROM sqlite_master WHERE type='table' AND name='data'";
+					$tableExistsResult = $dbFechas->querySingle($tableExistsQuery);
+
+					if ($tableExistsResult) {
+
+							$query = "SELECT file FROM data ORDER BY ROWID DESC LIMIT 1;";
+							$result = $dbFechas->query($query);
+
+							if ($result) {
+									while ($row = $result->fetchArray()) {
+											$lastFileInserted = $row["file"];
+											echo "<p>Último archivo insertado: " . $row["file"] . "</p>";
+									}
+									preg_match('/(\d+)/', $lastFileInserted, $number);
+									$fileNumberInserted = $number[0];
+									echo "<p>lastFileInserted: $lastFileInserted</p>";
+									echo "<p>fileNumberInserted: $fileNumberInserted</p>";
+							} else {
+								echo "<p>No se pudieron recuperar datos de la tabla.</p>";
+							}
+					} else {
+						$fileNumberInserted = 0;
+						echo "<p>La tabla no existe en la base de datos.</p>";
+					}
+
+					/* ########################## COMPARAR ARCHIVO ORIGEN CON ARCHIVO INSERTADO ########################## */
+
+					if ( $fileNumberInserted < $fileNumberOrigen) {
+
+						/* ################################### OBTENER RANGO PARA MT512E ################################### */
+
+						$mt512eDataInicial = null;
+						$mt512eDataFinal = null;
+
+						$query="SELECT data FROM mt512elog ORDER BY data LIMIT 1";
+						$result = $dbOrigen->query($query);
+						while ($row = $result->fetchArray()){
+							$mt512eDataInicial = $row["data"];
+							echo "<p>Primero mt512elog: " . $row["data"] . "</p>";
+						};
+
+						$query = "SELECT data FROM mt512elog ORDER BY data DESC LIMIT 1";
+						$result = $dbOrigen->query($query);
+						while ($row = $result->fetchArray()){
+							$mt512eDataFinal = $row["data"];
+							echo "<p>Último mt512elog: " . $row["data"] . "</p>";
+						};
+
+						/* ################################################################################################# */
+
+						/* ################################### OBTENER RANGO PARA TC900 ################################### */
+
+						$tc900DataInicial = null;
+						$tc900DataFinal = null;
+
+						$query = "SELECT data FROM tc900log ORDER BY data LIMIT 1";
+						$result = $dbOrigen->query($query);
+						while ($row = $result->fetchArray()){
+							$tc900DataInicial = $row["data"];
+							echo "<p>Primero tc900log: " . $row["data"] . "</p>";
+						};
+
+						$query = "SELECT data FROM tc900log ORDER BY data DESC LIMIT 1";
+						$result = $dbOrigen->query($query);
+						while ($row = $result->fetchArray()){
+							$tc900DataFinal = $row["data"];
+							echo "<p>Último tc900log: " . $row["data"] . "</p>";
+						};
+
+						$dbOrigen->close();
+
+						/* ################################################################################################# */
+
+						/* ############################## INSERTAR LOS RANGOS EN TABLA FECHAS ############################## */
+
+						$createTableQuery = "
+								CREATE TABLE IF NOT EXISTS data (
+										file TEXT,
+										mt512eDataInicial REAL,
+										mt512eDataFinal REAL,
+										tc900DataInicial REAL,
+										tc900DataFinal REAL
+								)
+						";
+						$dbFechas->exec($createTableQuery);
+
+						$queryInsert="INSERT INTO data (file, mt512eDataInicial, mt512eDataFinal, tc900DataInicial, tc900DataFinal) VALUES (:file, :mt512eDataInicial, :mt512eDataFinal, :tc900DataInicial, :tc900DataFinal)";
+
+						$stmt = $dbFechas->prepare($queryInsert);
+						$stmt->bindValue(':file', $file, SQLITE3_TEXT);
+						$stmt->bindValue(':mt512eDataInicial', $mt512eDataInicial, SQLITE3_FLOAT);
+						$stmt->bindValue(':mt512eDataFinal', $mt512eDataFinal, SQLITE3_FLOAT);
+						$stmt->bindValue(':tc900DataInicial', $tc900DataInicial, SQLITE3_FLOAT);
+						$stmt->bindValue(':tc900DataFinal', $tc900DataFinal, SQLITE3_FLOAT);
+						$stmt->execute();
+
+						/* ################################################################################################# */
+
+					};
+					$i++;
+				}
 			};
-			$i++;
-		};
-
-		// Conexión a la base de datos de origen
-		$dbOrigen = new SQLite3($path . "/" . $file);
-
-
-
-
-
-
-		$queryTc900Inicial = "SELECT data FROM tc900log ORDER BY data LIMIT 1";
-		$resultTc900Inicial = $dbOrigen->query($queryTc900Inicial);
-
-		while ($row=$resultTc900Inicial->fetchArray()){
-			$tc900DataInicial = $row["data"];
-      echo "<p>Primero tc900log: " . $row["data"] . "</p>";
-    };
-
-		$queryTc900Final = "SELECT data FROM tc900log ORDER BY data DESC LIMIT 1";
-		$resultTc900Final = $dbOrigen->query($queryTc900Final);
-
-		while ($row = $resultTc900Final->fetchArray()){
-			$tc900DataFinal = $row["data"];
-      echo "<p>Último tc900log: " . $row["data"] . "</p>";
-    };
-
-
-
-
-
-		$queryMt512eInicial="SELECT data FROM mt512elog ORDER BY data LIMIT 1";
-		$resultMt512etInicial = $dbOrigen->query($queryMt512eInicial);
-
-		while ($row=$resultMt512etInicial->fetchArray()){
-			$mt512eDataInicial = $row["data"];
-      echo "<p>Primero mt512elog: " . $row["data"] . "</p>";
-    };
-
-		$queryMt512eFinal = "SELECT data FROM mt512elog ORDER BY data DESC LIMIT 1";
-		$resultMt512eFinal = $dbOrigen->query($queryMt512eFinal);
-
-		while ($row = $resultMt512eFinal->fetchArray()){
-			$mt512eDataFinal = $row["data"];
-      echo "<p>Último mt512elog: " . $row["data"] . "</p>";
-    };
-
-
-    $dbFechas = new SQLite3('basesDestino/dbFechas.db');
-    $createTableQuery = "
-        CREATE TABLE IF NOT EXISTS data (
-            file TEXT,
-            mt512eDataInicial REAL,
-						mt512eDataFinal REAL,
-						tc900DataInicial REAL,
-						tc900DataFinal REAL
-        )
-    ";
-		// Ejecutar la sentencia para crear la tabla
-		$dbFechas->exec($createTableQuery);
-
-
-		$queryInsert="INSERT INTO data (file, mt512eDataInicial, mt512eDataFinal, tc900DataInicial, tc900DataFinal) VALUES (:file, :mt512eDataInicial, :mt512eDataFinal, :tc900DataInicial, :tc900DataFinal)";
-
-		$stmt = $dbFechas->prepare($queryInsert);
-		$stmt->bindValue(':file', "datos$fileNumber.db", SQLITE3_TEXT);
-		$stmt->bindValue(':mt512eDataInicial', $mt512eDataInicial, SQLITE3_FLOAT);
-		$stmt->bindValue(':mt512eDataFinal', $mt512eDataFinal, SQLITE3_FLOAT);
-		$stmt->bindValue(':tc900DataInicial', $tc900DataInicial, SQLITE3_FLOAT);
-		$stmt->bindValue(':tc900DataFinal', $tc900DataFinal, SQLITE3_FLOAT);
-		$stmt->execute();
-
-		// Realizar la consulta en la base de datos de origen
-/* 		$query2="SELECT * data FROM data ORDER BY data DESC";
-		$result2 = $dbFechas->query($query);
-
-		while ($row=$result2->fetchArray()){
-
-			//Mostrar en pantalla los datos originales
-			echo "<tr>";
-			echo "<td>" . $row["file"] . "</td>";
-			echo "<td>" . $row["dataInicial"] . "</td>";
-			echo "<td>" . $row["dataFinal"] . "</td>";
-			echo "</tr>";
-		}; */
+		} else {
+			echo "No se encontraron archivos en el directorio.";
+		}
 	?>
 
 	<h1>Reportes SITRAD v1.0.0</h1>
