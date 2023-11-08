e25dab71e27a75387e4dd83ce7612f80a563cc96
Maria Belen De Franchi <belendefranchi@gmail.com>
2023-11-08 09:58:14 -0300

    termino dbSensores

--- controllers/dbSensores.controller.php
+++ controllers/dbSensores.controller.php
@@ -5,141 +5,127 @@
 <title>Reportes SITRAD</title>
 <!-- <link rel="stylesheet" type="text/css" href="hoja2.css"> -->
 <link rel="icon" href="favicon.ico">
-
-
 </head>
 
 <body>
   <?php
-  $sensor = $_GET['sensor'];
-  echo $sensor;
-
-	// Conecta a la base de datos sensores
-	$dbSensores = new SQLite3('./basesDestino/dbSensores.db');
-
-  $createTableQuery = "
-    CREATE TABLE IF NOT EXISTS sensores (
-        id INTEGER,
-        modelo TEXT,
-        tipo TEXT,
-        nombre TEXT
-      )
-    ";
-
-  // Ejecutar la sentencia para crear la tabla
-  $dbSensores->exec($createTableQuery);
-
-  /* ############################### OBTENER EL ULTIMO ARCHIVO MODIFICADO ############################### */
-  $path = "./basesOrigen";
-  $dir = opendir($path);
-
-  while ($file = readdir($dir)) {
-    if ($file == "." || $file == "..") {
-      echo " ";
-    } else {
-      $files[$file] = filemtime($path . "/" . $file);
-    }
-  }
-
-  if (!empty($files)) {
-    //Ordena desde el más reciente.
-    arsort($files);
-
-    $i = 0;
-    foreach ($files as $file => $timestamp) {
-      if ($i < 1) {
-
-        $dbOrigen = new SQLite3($path . "/" . $file);
-        echo $file;
-        $query="SELECT id, modelo, descricao FROM instrumentos";
-        $result = $dbOrigen->query($query);
-        while ($data=$result->fetchArray()){
-          $id = $data["id"];
-          $modelo = $data["modelo"];
-          $nombre = $data["descricao"];
-
-          $queryInsert = "INSERT INTO data (id, modelo, nombre) VALUES (:id, :modelo, :nombre)";
-
-          $stmt = $dbSensores->prepare($queryInsert);
-          $stmt->bindValue(':id', $id, SQLITE3_TEXT);
-          $stmt->bindValue(':modelo', $modelo, SQLITE3_FLOAT);
-          /* $stmt->bindValue(':tipo', $tipo, SQLITE3_FLOAT); */
-          $stmt->bindValue(':nombre', $nombre, SQLITE3_FLOAT);
-          $stmt->execute();
-
-          echo "<tr>";
-          echo "<td>" . $data["id"] . "</td>";
-          echo "<td>" . $data["modelo"] . "</td>";
-          echo "<td>" . $data["descricao"] . "</td>";
-          echo "</tr>";
-        };
-        $dbOrigen->close();
-
-
-
-        $i++;
-      }
-    };
-  } else {
-  echo "No se encontraron archivos en el directorio.";
-  };
-
 
   echo '<a href="./index.php"><button>Volver</button></a>';
   echo '<h2>Tabla mt512e</h2>';
-  echo '<table width="80%">';
-  echo '<tr>';
-  echo '<td class="primera_fila">Id</td>';
-  echo '<td class="primera_fila">Temperatura</td>';
-  echo '<td class="primera_fila">Fecha y hora</td>';
-  echo '<td class="primera_fila">Fecha y hora</td>';
-  echo '</tr>';
 
-  // Conexión a la base de datos de origen
-/*   $dbOrigen = new SQLite3('../basesOrigen/datos89.db');
+  $dbSensores = new SQLite3('./basesDestino/dbSensores.db');
 
-  // Conexión a la base de datos de destino
-  $dbDestino = new SQLite3('../basesDestino/datos89.db');
+  $tableExistsQuery = "SELECT name FROM sqlite_master WHERE type='table' AND name='sensores'";
+	$tableExistsResult = $dbSensores->querySingle($tableExistsQuery);
 
-  // Sentencia SQL para crear la tabla en la base de datos de destino si no existe
-  $createTableQuery = "
-      CREATE TABLE IF NOT EXISTS mt512elog (
+	if (!$tableExistsResult) {
+
+    echo '<table width="80%">';
+    echo '<tr>';
+    echo '<td class="primera_fila">Id</td>';
+    echo '<td class="primera_fila">Modelo</td>';
+    echo '<td class="primera_fila">Sensor</td>';
+    echo '<td class="primera_fila">Nombre</td>';
+    echo '</tr>';
+    // Conecta a la base de datos sensores
+    $dbSensores = new SQLite3('./basesDestino/dbSensores.db');
+
+    $createTableQuery = "
+      CREATE TABLE IF NOT EXISTS sensores (
           id INTEGER,
-          Temperatura TEXT,
-          data REAL
-      )
-  ";
+          modelo TEXT,
+          tipo TEXT,
+          nombre TEXT
+        )
+      ";
 
-  // Ejecutar la sentencia para crear la tabla
-  $dbDestino->exec($createTableQuery);
+    // Ejecutar la sentencia para crear la tabla
+    $dbSensores->exec($createTableQuery);
 
-  // Realizar la consulta en la base de datos de origen
-  $query="SELECT id, Temperatura, data FROM mt512elog ORDER BY data DESC";
-  $result = $dbOrigen->query($query);
+    /* ############################### OBTENER EL ULTIMO ARCHIVO MODIFICADO ############################### */
+    $path = "./basesOrigen";
+    $dir = opendir($path);
 
+    while ($file = readdir($dir)) {
+      if ($file == "." || $file == "..") {
+        echo ' ';
+      } else {
+        $files[$file] = filemtime($path . "/" . $file);
+      }
+    }
 
-  // Recorrer los resultados y copiarlos a la base de datos de destino
-  while ($row=$result->fetchArray()){
+    if (!empty($files)) {
+      //Ordena desde el más reciente.
+      arsort($files);
 
-    $queryInsert="INSERT INTO mt512elog (id, Temperatura, data) VALUES (:id, :temp, :data)";
-    $stmt = $dbDestino->prepare($queryInsert);
-    $stmt->bindValue(':id', $row['id'], SQLITE3_INTEGER);
-    $stmt->bindValue(':temp', $row['Temperatura'], SQLITE3_TEXT);
-    $stmt->bindValue(':data', $row['data'], SQLITE3_FLOAT);
-    $stmt->execute();
+      $i = 0;
+      foreach ($files as $file => $timestamp) {
+        if ($i < 1) {
 
-    //Mostrar en pantalla los datos originales
-    echo "<tr>";
-    echo "<td>" . $row["id"] . "</td>";
-    echo "<td>" . $row["Temperatura"]/10 . "</td>";
-    echo "<td>" . $row["data"] . "</td>";
-    echo "<td>" . date('d/m/Y H:i:s', ($row["data"]-25569) * 86400) . "</td>";
-    echo "</tr>";
-  };
-  echo "</table>";
-  $dbOrigen->close();
-  $dbDestino->exec('VACUUM');
-  $dbDestino->close(); */
+          $dbOrigen = new SQLite3($path . "/" . $file);
+          $query="SELECT id, modelo, descricao FROM instrumentos";
+          $result = $dbOrigen->query($query);
+          while ($data=$result->fetchArray()){
+            $id = $data["id"];
+            $modelo = $data["modelo"];
+            $nombre = $data["descricao"];
+            if($modelo === 73){
+              $tipo = "mt512e";
+            } else {
+              $tipo = "tc900";
+            };
+
+            $queryInsert = "INSERT INTO sensores (id, modelo, tipo, nombre) VALUES (:id, :modelo, :tipo, :nombre)";
+
+            $stmt = $dbSensores->prepare($queryInsert);
+            $stmt->bindValue(':id', $id, SQLITE3_TEXT);
+            $stmt->bindValue(':modelo', $modelo, SQLITE3_FLOAT);
+            $stmt->bindValue(':tipo', $tipo, SQLITE3_TEXT);
+            $stmt->bindValue(':nombre', $nombre, SQLITE3_TEXT);
+            $stmt->execute();
+
+            echo '<tr>';
+            echo '<td>' . $data["id"] . '</td>';
+            echo '<td>' . $data["modelo"] . '</td>';
+            echo '<td>' . $tipo . '</td>';
+            echo '<td>' . $data["descricao"] . '</td>';
+            echo '</tr>';
+          };
+          $dbOrigen->close();
+          $i++;
+        }
+      };
+      echo '</table>';
+    } else {
+    echo 'No se encontraron archivos en el directorio.';
+    };
+
+  } else {
+
+    echo '<table width="80%">';
+    echo '<tr>';
+    echo '<td class="primera_fila">Id</td>';
+    echo '<td class="primera_fila">Modelo</td>';
+    echo '<td class="primera_fila">Sensor</td>';
+    echo '<td class="primera_fila">Nombre</td>';
+    echo '</tr>';
+
+    $query="SELECT id, modelo, tipo, nombre FROM sensores";
+    $result = $dbSensores->query($query);
+
+    while ($data=$result->fetchArray()){
+      echo '<tr>';
+      echo '<td>' . $data["id"] . '</td>';
+      echo '<td>' . $data["modelo"] . '</td>';
+      echo '<td>' . $data["tipo"] . '</td>';
+      echo '<td>' . $data["nombre"] . '</td>';
+      echo '</tr>';
+    };
+    echo '</table>';
+  }
+
+  $dbSensores->close();
+
   ?>
 </body>
 </html>
